470b7cc88e9b297aa789415d0e88777b3e2fcdff
weckest <lpeder@shaw.ca>
2024-05-17 13:54:32 -0700

    minor updates to the shop page and inventory

--- shopRouter.js
+++ shopRouter.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const ejs = require('ejs');
 
 router.use(express.urlencoded({extended: false}));
-const {purchaseItem, coinDistribution} = require('./game');
+const {purchaseItem, coinDistribution, resetCoinsReceived} = require('./game');
 
 
 
 module.exports = function(itemCollection, userCollection) {
     router.get('/shop', async (req, res) => {
+        resetCoinsReceived();
         coinDistribution(req, 'hexagon');
         if(req.session.authenticated) {
             if(req.session.shop == null) {
@@ -82,9 +83,6 @@ module.exports = function(itemCollection, userCollection) {
         }
 
         result[0].itemList[result[0].itemList.length] = item;
-        if(!purchaseItem(req, item)){
-           
-        }
 
         res.redirect('/shop');
     });
