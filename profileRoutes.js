73f2be86ef783d901d037b22636da26f26f02335
Tom He <the18@my.bcit.ca>
2024-05-28 12:15:24 -0700

    Updated defaults setting on signup to help new users know how to customize their profile

--- profileRoutes.js
+++ profileRoutes.js
@@ -3,81 +3,76 @@ const router = express.Router();
 
 
 
-module.exports = function(userCollection, userTitlesCollection) {
-    router.get('/profile', async (req, res) => {
-        const from = req.query.from;
-        res.locals.gameStarted = req.session.gameSession ? true : false;
-        if (req.session.authenticated) {
-            const userName = req.session.username;
-            const userEmail = req.session.email;
-            const userId = req.session.userId;
+module.exports = function (userCollection, userTitlesCollection) {
+  router.get('/profile', async (req, res) => {
+    const from = req.query.from;
+    res.locals.gameStarted = req.session.gameSession ? true : false;
+    if (req.session.authenticated) {
+      const userName = req.session.username;
+      const userEmail = req.session.email;
+      const userId = req.session.userId;
 
-            const user = await userCollection.findOne({ username: userName });
-            const userProfilePic = user ? user.profile_pic : 'profile-logo.png';
-            const friendsList = user.friendsList; 
-            const userBio = user.bio;
-            const userTitle = user.UserTitle;
-            const ownedProfilePics = user ? user.ownedProfilePics : [];
-            const goldCollected = user ? user.goldCollected : 0;
-            const runsCompleted = user ? user.runsCompleted : 0;
-            const damageDealt = user ? user.totalDamageDealt : 0;
+      const user = await userCollection.findOne({ username: userName });
+      const userProfilePic = user ? user.profile_pic : 'profile-logo.png';
+      const friendsList = user.friendsList;
+      const userBio = user.bio;
+      const userTitle = user.UserTitle;
+      const ownedProfilePics = user ? user.ownedProfilePics : [];
+      const goldCollected = user ? user.goldCollected : 0;
+      const runsCompleted = user ? user.runsCompleted : 0;
+      const damageDealt = user ? user.totalDamageDealt : 0;
 
-            console.log(ownedProfilePics);
+      console.log(ownedProfilePics);
 
-            const ownedUserTitles = user ? user.titles : [];
+      const ownedUserTitles = user ? user.titles : [];
 
-            console.log(ownedUserTitles);
-            
-            res.render("profile", { goldCollected: goldCollected, runsCompleted: runsCompleted, damageDealt: damageDealt, userName: userName, userEmail: userEmail, userProfilePic: userProfilePic, userId: userId, friendsList: friendsList, userTitle: userTitle, userBio: userBio, ownedProfilePics: ownedProfilePics, ownedUserTitles: ownedUserTitles, from: from });
-        } else {
-            res.redirect('/login');
-        }
-    });
+      console.log(ownedUserTitles);
 
-    router.use(express.json());
+      res.render("profile", { goldCollected: goldCollected, runsCompleted: runsCompleted, damageDealt: damageDealt, userName: userName, userEmail: userEmail, userProfilePic: userProfilePic, userId: userId, friendsList: friendsList, userTitle: userTitle, userBio: userBio, ownedProfilePics: ownedProfilePics, ownedUserTitles: ownedUserTitles, from: from });
+    } else {
+      res.redirect('/login');
+    }
+  });
 
-    router.post('/updateProfilePicture', async (req, res) => {
-        console.log("Updating profile picture...");
-        const { profilePictureUrl } = req.body;
-    
-        try {
-            const userName = req.session.username;
-            
-            await userCollection.updateOne({ username: userName }, { $set: { profile_pic: profilePictureUrl } });
-            req.session.profile_pic = profilePictureUrl;
-            res.sendStatus(200); 
-        } catch (error) {
-            console.error('Error updating profile picture:', error);
-            res.sendStatus(500); 
-        }
-    });
+  router.use(express.json());
 
-    router.post('/updateProfile', async (req, res) => {
-        console.log("Updating profile...");
-        const { title, bio } = req.body;
-        
-        try {
-            const userName = req.session.username;
+  router.post('/updateProfilePicture', async (req, res) => {
+    const { profilePictureUrl } = req.body;
 
-            await userCollection.updateOne(
-                { username: userName },
-                { $set: { UserTitle: title, bio: bio } }
-            );
-            console.log("Profile updated successfully");
-    
-            res.status(200).send({ success: true, message: 'Profile updated successfully' });
-        } catch (error) {
-            console.error('Error updating profile:', error);
-            res.status(500).send({ success: false, message: 'Failed to update profile' });
-        }
-    });
+    try {
+      const userName = req.session.username;
+      await userCollection.updateOne({ username: userName }, { $set: { profile_pic: profilePictureUrl } });
+      req.session.profile_pic = profilePictureUrl;
+      res.sendStatus(200);
+    } catch (error) {
+      console.error('Error updating profile picture:', error);
+      res.sendStatus(500);
+    }
+  });
 
-    router.get('/logout', (req,res) => {
-        console.log("logging out")
-        req.session.destroy();
-        res.redirect('/');
-    });
-    
+  router.post('/updateProfile', async (req, res) => {
+    console.log("Updating profile...");
+    const { title, bio } = req.body;
 
-    return router;
+    try {
+      const userName = req.session.username;
+      await userCollection.updateOne(
+        { username: userName },
+        { $set: { UserTitle: title, bio: bio } }
+      );
+      res.status(200).send({ success: true, message: 'Profile updated successfully' });
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      res.status(500).send({ success: false, message: 'Failed to update profile' });
+    }
+  });
+
+  router.get('/logout', (req, res) => {
+    console.log("logging out")
+    req.session.destroy();
+    res.redirect('/');
+  });
+
+
+  return router;
 };
